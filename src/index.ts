3eaaf422ee17e4eb364f0b03fac2ccbfeb635a1e
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-08 16:03:07 +0800

    feat: add more options

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,21 @@ function generateNames(str: string) {
   const strs = str.split(' ')
   if (strs.length === 1) {
     const lowStr = str.toLowerCase()
-    return [lowStr, `_${lowStr}`, `_${lowStr}_`, `${lowStr}$`, `str${lowStr[0].toUpperCase()}${lowStr.slice(1)}`, `int${lowStr[0].toUpperCase()}${lowStr.slice(1)}`]
+    return [
+      lowStr,
+      `_${lowStr}`,
+      `_${lowStr}_`,
+      `${lowStr}$`,
+      `str${lowStr[0].toUpperCase()}${lowStr.slice(1)}`,
+      `num${lowStr[0].toUpperCase()}${lowStr.slice(1)}`,
+      `bool${lowStr[0].toUpperCase()}${lowStr.slice(1)}`,
+      `arr${lowStr[0].toUpperCase()}${lowStr.slice(1)}`,
+      `obj${lowStr[0].toUpperCase()}${lowStr.slice(1)}`,
+      `int${lowStr[0].toUpperCase()}${lowStr.slice(1)}`,
+      `string${lowStr[0].toUpperCase()}${lowStr.slice(1)}`,
+      `float${lowStr[0].toUpperCase()}${lowStr.slice(1)}`,
+      `double${lowStr[0].toUpperCase()}${lowStr.slice(1)}`,
+    ]
   }
   // 1. _
   result.push(strs.reduce((pre, cur) => `${pre.toLowerCase()}_${cur.toLowerCase()}`))
@@ -67,10 +81,24 @@ function generateNames(str: string) {
   result.push(strs.reduce((pre, cur, i) => `${i === 1 ? '_' : ''}${pre.toLowerCase()}__${cur.toLowerCase()}`))
   // 7. 每个单词首字母都大写
   result.push(strs.reduce((pre, cur) => `${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
-  // 8. str在变量名前加上数据类型或其他标识符的缩写
-  result.push(strs.reduce((pre, cur) => `str${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
-  // 9. str在变量名前加上数据类型或其他标识符的缩写
+  // 8. string在变量名前加上数据类型或其他标识符的缩写
+  result.push(strs.reduce((pre, cur) => `string${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
+  // 9. int在变量名前加上数据类型或其他标识符的缩写
   result.push(strs.reduce((pre, cur) => `int${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
+  // 10. float在变量名前加上数据类型或其他标识符的缩写
+  result.push(strs.reduce((pre, cur) => `float${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
+  // 11. double在变量名前加上数据类型或其他标识符的缩写
+  result.push(strs.reduce((pre, cur) => `double${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
+  // 12. str在变量名前加上数据类型或其他标识符的缩写
+  result.push(strs.reduce((pre, cur) => `str${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
+  // 13. num在变量名前加上数据类型或其他标识符的缩写
+  result.push(strs.reduce((pre, cur) => `num${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
+  // 14. bool在变量名前加上数据类型或其他标识符的缩写
+  result.push(strs.reduce((pre, cur) => `bool${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
+  // 15. arr在变量名前加上数据类型或其他标识符的缩写
+  result.push(strs.reduce((pre, cur) => `arr${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
+  // 15. obj在变量名前加上数据类型或其他标识符的缩写
+  result.push(strs.reduce((pre, cur) => `obj${pre[0].toUpperCase()}${pre.slice(1).toLowerCase()}${cur[0].toUpperCase()}${cur.slice(1).toLowerCase()}`))
 
   return result
 }
